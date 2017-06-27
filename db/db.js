a2d95b8f3989729ddc8f2e69c20049d0eaa7043e
rivermanvr <vrgithub@riversconsulting.com>
2017-06-27 12:23:48 -0400

    s/u alternative ways for defining db connection

--- db/db.js
+++ db/db.js
@@ -2,4 +2,22 @@ const Sequelize = require( 'sequelize' );
 const connectDB = process.env.DATABASE_URL || 'postgres://localhost/acme_sql';
 const db = new Sequelize(connectDB, { logging: false });
 
+
+// alternative way to setup connection:
+/*
+ const db = new Sequelize('acme_sql', 'userVin', 'pass123', {
+   host: 'localhost',
+   dialect: 'postgres'
+ });
+
+or
+
+ const db = new Sequelize('acme_sql', null, null, {
+   host: 'localhost',
+   dialect: 'postgres',
+   logging: false
+ });
+
+*/
+
 module.exports = db;
